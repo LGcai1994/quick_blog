97aa7a1c7d41a85e3ddaecfd83abbfc95df28e3d
LGcai1994 <cailigang1994@outlook.com>
2024-04-17 15:23:41 +0800

    basic ok

---
+++ src/components/AddArticle.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import { Timestamp, collection, addDoc } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage, db, auth } from "./../firebaseConfig";
+import { toast } from "react-toastify";
+
+export default function AddArticle() {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    image: '',
+    createdAt: Timestamp.now().toDate()
+  })
+  const [progress, setProgress] = useState(0)
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+  const handleImgChange = (e) => {
+    setFormData({ ...formData, image: e.target.files[0] })
+  }
+  const handlePublish = () => {
+    if (!formData.title || !formData.description || !formData.image) {
+      alert("Please fill all the fields");
+      return;
+    }
+    // 对应于用户上传图片的存储引用 (storageRef)
+    const storageRef = ref(storage, `/images/${Date.now()}${formData.image.name}`);
+    // 将图片上传到 Firebase Storage 中
+    const uploadImage = uploadBytesResumable(storageRef, formData.image);
+    // 监听上传状态
+    uploadImage.on("state_changed",
+      // 回调函数计算上传进度
+      (snapshot) => {
+        const progressPercent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progressPercent);
+      },
+      // 处理错误回调函数
+      (err) => {
+        console.log(err);
+      },
+      // 上传完毕回调函数
+      () => {
+        setFormData({
+          title: "",
+          description: "",
+          image: "",
+        });
+        getDownloadURL(uploadImage.snapshot.ref).then((url) => {
+          // 创建对应于文章的数据库引用，并保存文章信息到 Firestore 中
+          const articleRef = collection(db, "Articles");
+          addDoc(articleRef, {
+            title: formData.title,
+            description: formData.description,
+            imageUrl: url,
+            createdAt: Timestamp.now().toDate(),
+            // createdBy: user.displayName,
+            // userId: user.uid,
+            likes: [],
+            comments: []
+          })
+            .then(() => {
+              // 显示成功添加文章的提示消息，并重置上传进度为0
+              toast("Article added successfully", { type: "success" });
+              setProgress(0);
+            })
+            .catch((err) => {
+              // 如果保存文章时出现错误，显示错误提示消息
+              toast("Error adding article", { type: "error" });
+            });
+        });
+      }
+    );
+  };
+
+  return (
+    <div className='border p-3 mt-3 bg-light' style={{ position: 'fixed' }}>
+
+      {/* title input */}
+      <h2>Create Title</h2>
+      <label htmlFor=''>Title</label>
+      <input
+        type='text'
+        name='title'
+        className='form-control'
+        value={formData.title}
+        onChange={(e) => handleChange(e)} />
+
+      {/* description input */}
+      <label htmlFor="">Description</label>
+      <textarea
+        name='description'
+        className='form-control'
+        value={formData.description}
+        onChange={(e) => handleChange(e)} />
+
+      {/* picture input */}
+      <label htmlFor=''>Picture</label>
+      <input
+        type='file'
+        name='image'
+        accept='image/*'
+        className='form-control'
+        onChange={(e) => handleImgChange(e)} />
+
+      {/* progress bar */}
+      <div className='progress'>
+        <div className='progress-bar progress-bar-striped mt-2' style={{ width: '50%' }}>
+          50%
+        </div>
+      </div>
+
+      {/* publish button */}
+      <button
+        className='form-control btn-primary mt-2'
+        onClick={handlePublish}>
+        Publish
+      </button>
+
+    </div>
+  )
+}
---
+++ src/components/Articles.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebaseConfig'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+
+export default function Articles() {
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        const articleRef = collection(db, 'Articles')
+        const q = query(articleRef, orderBy('createdAt', 'desc'))
+        onSnapshot(q, (snapshot) => {
+            const articles = snapshot.docs.map((doc) => (
+                {
+                    id: doc.id,
+                    ...doc.data()
+                }
+            ))
+            setArticles(articles)
+        })
+    }, [])
+
+    return (
+        <div>
+            {
+                articles.length === 0
+                    ?
+                    (<p>No article found</p>)
+                    :
+                    (
+                        articles.map(({ id, title, description, imageUrl, createdAt }) => (
+                            <div key={id} className='border mt-3 p-3 bg-light'>
+                                <div className='row'>
+                                    <div className='col-4'>
+                                        <img src={imageUrl} alt='blog picture' style={{ height: 180, width: 180 }} />
+                                    </div>
+                                    <div className='col-8 ps-3'>
+                                        <h2>{title}</h2>
+                                        <p>{createdAt.toDate().toDateString()}</p>
+                                        <h4>{description}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    )
+            }
+        </div>
+    )
+}
